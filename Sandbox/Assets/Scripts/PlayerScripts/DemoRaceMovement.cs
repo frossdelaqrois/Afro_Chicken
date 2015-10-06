e7010f67c7b56cf1b9402a3e176b0834034a0856
Daniel Aherne <frossdelaqrois@gmail.com>
2015-10-06 03:14:36 +0100

    updated scripts in main Scene started enabling our player controller for multiplayer

---
+++ Sandbox/Assets/Scripts/PlayerScripts/DemoRaceMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoRaceMovement : MonoBehaviour 
+{
+    public ThirdPersonOrbitCam Camera;
+
+    void OnJoinedRoom()
+    {
+        CreatePlayerObject();
+    }
+
+    void CreatePlayerObject()
+    {
+        Vector3 position = new Vector3( 0f, 0f, 0f );
+
+        GameObject newPlayerObject = PhotonNetwork.Instantiate( "Chicken", position, Quaternion.identity, 0 );
+
+       
+    }
+}

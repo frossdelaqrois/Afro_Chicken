cfb79f83e44fd2afa7a58982388d54f17398f166
Daniel Aherne <frossdelaqrois@gmail.com>
2015-10-14 22:48:08 +0100

    Added new project removed old projects

---
+++ Basic_Movement/Assets/player.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+   public Vector3 speed = new Vector3(0,0,5);
+   
+ 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("key was pressed");
+            transform.Translate(speed*Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("key was pressed");
+            transform.Translate(Vector3.left * Time.deltaTime*5);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("key was pressed");
+            transform.Translate(Vector3.right * Time.deltaTime*5);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("key was pressed");
+            transform.Translate(Vector3.back * Time.deltaTime*5);
+        }
+
+    }
+}
